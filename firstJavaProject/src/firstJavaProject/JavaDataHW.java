27e3c545e4ec83eb8e79c6ff0415cc86d042f1e6
NJITneg5 <neg5@njit.edu>
2020-09-16 12:38:35 -0400

    Added Java Data HW

---
+++ firstJavaProject/src/firstJavaProject/JavaDataHW.java
@@ -0,0 +1,18 @@
+package firstJavaProject;
+
+public class JavaDataHW {
+	public static void main(String[] args) {
+		
+		String myName = "Nate";
+		
+		System.out.println("Hi, my name is " + myName + ".");
+		
+		int myAge = 21;
+		
+		System.out.println("I am " + myAge + " years old.");
+		
+		double myHeight = 5.75;
+		
+		System.out.println("I am "+ myHeight + " feet tall.");
+	}
+}
