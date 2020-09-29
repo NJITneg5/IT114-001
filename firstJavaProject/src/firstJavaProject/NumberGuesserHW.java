0ba90a1ff3d9da9d8bfd49b128cc1079abc1097f
NJITneg5 <neg5@njit.edu>
2020-09-29 17:30:49 -0400

    Added NumberGuesserHW stuff

--- firstJavaProject/src/firstJavaProject/NumberGuesserHW.java
+++ firstJavaProject/src/firstJavaProject/NumberGuesserHW.java
@@ -14,7 +14,13 @@ public class NumberGuesserHW {
 	private int number = 0;
 	private boolean isRunning = false;
 	final String saveFile = "numberGuesserSave.txt";
+	
+	private int levelMultiplier = 71; 	//These numbers will be used to jumble up the save file. 
+	//Since they are prime, you will have to divide the levelHash by this exact number in order to get a valid level
+	
+	private int maxLevelHash = 7029; 	//This is 99*71. For ease, we will assume that the maximum level is 99
 
+	private String fool = "114"; 		//This has to be in the level save in order for it to load									
 	/***
 	 * Gets a random number between 1 and level.
 	 * 
@@ -53,6 +59,40 @@ public class NumberGuesserHW {
 			System.out.println("Tired of playing? No problem, see you next time.");
 			isRunning = false;
 		}
+		//CHEATS
+		
+		if (message.equalsIgnoreCase("change strikes"))
+		{
+			try (Scanner input = new Scanner(System.in);) {
+				int change = input.nextInt();
+				strikes = change;
+				message = "" + change;
+			}
+			catch(Exception e) {
+				return;
+			}
+		}
+		
+		if (message.equalsIgnoreCase("change max strikes"))
+		{
+			try (Scanner input = new Scanner(System.in);)
+			{
+				int change = input.nextInt();
+				maxStrikes = change;
+				message = "" + change;
+			}
+			catch(Exception e) {
+				return;
+			}
+		}
+		
+		//FUNNY JOKE HOO-HA
+		if(message.equalsIgnoreCase(" ")) {
+			int range = 9 + ((level - 1) * 5);
+			int randomGuess = new Random().nextInt(range);
+			System.out.println("What are you doing?... I'll make a guess for you..." + randomGuess);
+			message = "" + randomGuess;
+		}
 	}
 
 	private void processGuess(int guess) {
@@ -71,9 +111,9 @@ public class NumberGuesserHW {
 				int remainder = maxStrikes - strikes;
 				System.out.println("You have " + remainder + "/" + maxStrikes + " attempts remaining");
 				if (guess > number) {
-					System.out.println("Lower");
+					System.out.println("Go Lower");
 				} else if (guess < number) {
-					System.out.println("Higher");
+					System.out.println("Go Higher");
 				}
 			}
 		}
@@ -91,8 +131,12 @@ public class NumberGuesserHW {
 	}
 
 	private void saveLevel() {
+		
+		int levelHash = level * levelMultiplier;
+		String saveHash = "" + levelHash + fool; //To make save file number look like a random number
+		
 		try (FileWriter fw = new FileWriter(saveFile)) {
-			fw.write("" + level);// here we need to convert it to a String to record correctly
+			fw.write("" + saveHash);// here we need to convert it to a String to record correctly
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -104,27 +148,40 @@ public class NumberGuesserHW {
 		if (!file.exists()) {
 			return false;
 		}
+		String loadHash;
+		
 		try (Scanner reader = new Scanner(file)) {
 			while (reader.hasNextLine()) {
-				int _level = reader.nextInt();
-				if (_level > 1) {
-					level = _level;
-					break;
+				loadHash = reader.nextLine();
+				int seperation = -1;
+				seperation = loadHash.indexOf(fool);
+				float levelHash = Float.parseFloat(loadHash.substring(0,seperation));
+				if (levelHash <= maxLevelHash && levelHash % levelMultiplier == 0) 
+				{ //Checks that the hash is under the threshold and that is divisible by the multiplier
+						level = (int) levelHash / levelMultiplier;
+						break;
 				}
 			}
-		} catch (FileNotFoundException e) {
+		}
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return false;
-		} catch (Exception e2) {
+		} 
+		catch(StringIndexOutOfBoundsException e3){
+			e3.printStackTrace();
+			return false;
+		}
+		catch (Exception e2) {
 			e2.printStackTrace();
 			return false;
 		}
+		
 		return level > 1;
 	}
 
 	void run() {
 		try (Scanner input = new Scanner(System.in);) {
-			System.out.println("Welcome to Number Guesser 4.0!");
+			System.out.println("Welcome to Number Guesser HW!");
 			System.out.println("I'll ask you to guess a number between a range, and you'll have " + maxStrikes
 					+ " attempts to guess.");
 			if (loadLevel()) {
